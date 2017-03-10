9018830722112daa651d0e4f58612f86cc3ccd0f
kabutz <heinz@javaspecialists.eu>
2017-03-10 08:58:53 +0200

    Accidentally committed partially changed file - undone

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -6,13 +6,9 @@ import java.util.concurrent.*;
 
 public class Fibonacci {
     public BigInteger f(int n) {
-
         if (n == 0) return BigInteger.ZERO;
         if (n == 1) return BigInteger.ONE;
 
-        int half = (n + 1) / 2;
-
-        BigInteger f0 = f(half - 1);
         return f(n-1).add(f(n-2));
     }
 }
